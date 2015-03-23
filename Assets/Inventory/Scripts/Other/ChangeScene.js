0ec94162a130ecf4c31133dbda16942a180dde4d
Chikari <charmyhon@hotmail.com>
2015-03-23 23:20:01 +0800

    Fix inventory system
    persistent item list between battle and map scene

--- Assets/Inventory/Scripts/Other/ChangeScene.js
+++ Assets/Inventory/Scripts/Other/ChangeScene.js
@@ -4,18 +4,21 @@
 
 function Awake () 
 {
-	//var temp : Transform;
-	//temp = GameObject.Find(PlayerName).transform;
-	//DontDestroyOnLoad (transform.root.temp);
+		var temp = GameObject.Find("Victim");
+		if (temp == null) Debug.Log("player not found");
+		DontDestroyOnLoad(temp);
+		
+		if (FindObjectsOfType(GetType()).Length > 1)
+         {
+             Destroy(gameObject);
+         }
 }
 
 function Update ()
 {
 	if (Input.GetKeyDown(KeyCode.T))
 	{
-		var temp = GameObject.Find("Victim");
-		if (temp == null) Debug.Log("player not found");
-		//DontDestroyOnLoad(temp);
+
 		Application.LoadLevel("battle");
 	}
 }
