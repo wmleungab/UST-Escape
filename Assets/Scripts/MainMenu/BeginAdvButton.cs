3bc3e4db038fff4dea50d2176d6798144e0bd621
wmleungab <wmleungab@ust.hk>
2015-04-07 13:47:59 +0800

    SL system in progress 2

--- Assets/Scripts/MainMenu/BeginAdvButton.cs
+++ Assets/Scripts/MainMenu/BeginAdvButton.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class BeginAdvButton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnMouseUp(){
+		renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 1f);
+		SaveLoadSystem.getInstance ().resetSave ();
+		Application.LoadLevel ("lab_stage"); 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseDown(){
+		renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0.5f);
 	}
 }
--- Assets/Scripts/MainMenu/LoadGameButton.cs
+++ Assets/Scripts/MainMenu/LoadGameButton.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class LoadGameButton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	void OnMouseUp(){
+		renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 1f);
+
+		SaveLoadSystem.getInstance ().load ();
+		if (SaveLoadSystem.getInstance ().currentSceneType==SaveLoadSystem.SceneType.LAB) {
+			Debug.Log ("MainMenu: Loading Lab scene");
+			Application.LoadLevel ("lab_stage"); 
+		}else if(SaveLoadSystem.getInstance ().currentSceneType==SaveLoadSystem.SceneType.ATRIUM) {
+			Application.LoadLevel ("atrium_stage"); 
+		}else if(SaveLoadSystem.getInstance ().currentSceneType==SaveLoadSystem.SceneType.LG2) {
+			Application.LoadLevel ("LG2_stage"); 
+		}else if(SaveLoadSystem.getInstance ().currentSceneType==SaveLoadSystem.SceneType.SUNDIAL) {
+			Application.LoadLevel ("sundial"); 
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnMouseDown(){
+		renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0.5f);
 	}
 }
