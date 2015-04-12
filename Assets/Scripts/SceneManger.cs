791e8af401aef2caed15846f4e236316d5772e84
Chikari <Chaomian@wf124-251.ust.hk>
2015-04-12 13:21:24 +0800

    battle background

---
+++ Assets/Scripts/SceneManger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class SceneManger {
+
+	static string currentScene = "";
+	static string nextScene = "";
+
+	static void changeScene(string sceneName){
+		currentScene = sceneName;
+		Application.LoadLevel (sceneName);
+	}
+
+	static void battleCallback(){
+
+	}
+
+}
