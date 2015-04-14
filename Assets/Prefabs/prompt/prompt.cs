f0c94f9da5e80a132773cec1796cc20c578e68d8
wmleungab <wmleungab@ust.hk>
2015-04-14 18:46:32 +0800

    bug fix in battle

---
+++ Assets/Prefabs/prompt/prompt.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prompt : MonoBehaviour {
+
+	// Use this for initialization
+
+	void Start(){
+		Invoke ("destroyprompt", 2.5f);
+	}
+	void destroyprompt(){
+		Destroy (gameObject);
+	}
+}
