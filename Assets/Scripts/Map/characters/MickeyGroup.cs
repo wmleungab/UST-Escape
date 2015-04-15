fae53e21e30d3778cac5f24bb9ea1dd2d3e40fa8
Chikari <charmyhon@hotmail.com>
2015-04-15 16:34:45 +0800

    Some of the items & bug fix

--- Assets/Scripts/Map/characters/MickeyGroup.cs
+++ Assets/Scripts/Map/characters/MickeyGroup.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class MickeyGroup : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+		if (SaveLoadSystem.getInstance ().lg2SceneStateArr [(int)SaveLoadSystem.Lg2SceneState.SAVEPRED]){
+			Destroy (this.gameObject);
+		}
+	}
+
 	
 	void groupBattle(){
 		GetComponent<sceneInterface>().gotoBattle();
 		Destroy(this.gameObject);
+		SaveLoadSystem.getInstance ().lg2SceneStateArr [(int)SaveLoadSystem.Lg2SceneState.BEGANDIALOG] = true;
+		SaveLoadSystem.getInstance ().save ();
 	}
 	
 }
